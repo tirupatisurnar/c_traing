c26c54383180d85ffde89213577e57eeb2de64d9
tirupatisurnar <129940897+tirupatisurnar@users.noreply.github.com>
2023-04-07 18:19:30 +0530

    Add files via upload
    frist program

---
+++ frist-program.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    // create a variable
+ int ts, ps; 
+ printf("enter age of tirupati: "); //geting user input
+ scanf("%d", &ts );scanning or storeing the input
+ printf("enter age of priti: "); asking the input
+ scanf("%d", &ps );
+ printf ("%d %d", ts, ps);
+ return 0;
+ 
+
+}
